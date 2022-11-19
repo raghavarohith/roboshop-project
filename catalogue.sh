4b65b9ed39e1d8e3af0ec160eb4bfed59629208f
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-19 23:45:48 +0400

    catalogue changes

--- catalogue.sh
+++ catalogue.sh
@@ -1,8 +1,24 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+  fi
 yum install nodejs -y
+if [ $? -eq 0 ]
+then
+echo SUCCESS
+fi
 useradd roboshop
+if [ $? -eq 0 ]; then
+echo SUCCESS
+fi
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+if [ $? -eq 0 ]; then
+echo SUCCESS
+fi
 cd /home/roboshop
+if [ $? -eq 0 ]; then
+echo SUCCESS
+fi
 unzip -o /tmp/catalogue.zip
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
