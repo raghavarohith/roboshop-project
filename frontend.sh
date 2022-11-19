349dc1ccec14fa1b6e19dbc260c55002a9fcd160
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-19 19:52:33 +0400

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,10 @@
 yum install nginx -y
+if [ $? -eq 0 ]
+then
 echo -e "\e[32m nginx SUCCESS in color\e[0m"
+else
+echo Failure
+fi
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
