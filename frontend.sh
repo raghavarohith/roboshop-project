3a018a9d7ab7d0f0d2d5718e293612c4b4f2b2af
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-19 19:43:03 +0400

    frontend

--- frontend.sh
+++ frontend.sh
@@ -4,7 +4,7 @@ echo "install nginx SUCCESS"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend.zip
+unzip -o /tmp/frontend.zip
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx
