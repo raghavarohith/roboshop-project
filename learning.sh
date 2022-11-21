891609ec3337ff06ae8dec5f35ac75162d07b84b
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-22 01:23:49 +0400

    learning script

--- learning.sh
+++ learning.sh
@@ -1,7 +1,10 @@
-read -p "enter my name:" pass
-read -p "enter your age:" pass2
-echo name=$pass
-echo age=$pass2
+#read -p "enter my name:" pass
+#read -p "enter your age:" pass2
+#echo name=$pass
+#echo age=$pass2
+
+read -s -p "enter your password" local
+echo password-$local
 
 
 
