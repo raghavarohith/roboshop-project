a047f74a5b1b95dd3687258da719d86c0e1ff583
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-22 23:57:53 +0400

    cart script

--- cart.sh
+++ cart.sh
@@ -1,21 +1,21 @@
 source common.sh
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash >/tmp/test
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash >/tmp/log.txt
 STAT
 if [ $? -ne 0 ]; then
 useradd roboshop
 fi
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" >/tmp/test
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" >/tmp/log.txt
 cd /home/roboshop
 rm -rf cart
-unzip -o /tmp/cart.zip >/tmp/test
+unzip -o /tmp/cart.zip >/tmp/log.txt
 mv cart-main cart
 cd cart
-npm install >/tmp/test
+npm install >/tmp/log.txt
 if [ $? -eq 0 ]; then
 echo -e "\e[33m SUCCESS NPM INSTALLED\e[0m"
 fi
 sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' systemd.service
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service >/tmp/test
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service >/tmp/log.txt
 systemctl daemon-reload
 systemctl enable cart
 systemctl restart cart
