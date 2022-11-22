322677a518fe429c38e7f02bb5c86c2df83b5066
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-23 00:48:34 +0400

    mysql script

--- mysql.sh
+++ mysql.sh
@@ -4,7 +4,8 @@ yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
 
-
+if [ $? -ne 0 ]; then
 echo "ALTER USER 'root'@'localhost' IDENTIFIED by 'RoboShop@1';" >/tmp/root-pass-sql
 DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 cat /tmp/root-pass-sql | mysql --connect-expired-password  -uroot -p"${DEFAULT_PASSWORD}"
+fi
