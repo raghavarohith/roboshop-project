cce4717daa3daf4ca5abf5291ddd3c611760f4d8
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-23 02:03:37 +0400

    my sql script

--- mysql.sh
+++ mysql.sh
@@ -1,50 +1,33 @@
-if [ -n "$1" ]; then
+if [ -z "$1" ]; then
   echo INPUT PASSWORD IS NEEDED
   exit
   fi
 
 ROBOSHOP_MYSQL_PASSWORD=$1
+STAT () {
+  if [ $1 -eq 0 ]
+  then
+    echo SUCCESS
+  else
+   echo FAILURE
+   fi
+}
 echo -e "\e[34mDOWNLOADING MYSQL REPO FILE\e[0m"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
- echo FAILURE
- fi
+STAT $?
  echo DISABLE MYSQL SERVICE
 dnf module disable mysql
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
- echo FAILURE
- fi
+STAT $?
  echo INSTALL MYSQL SERVICE
 yum install mysql-community-server -y
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
- echo FAILURE
-
- fi
+STAT $?
  echo MYSQL SERVICE ENABLE
 systemctl enable mysqld
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
- echo FAILURE
- fi
+STAT $?
+
  echo MYSQL SERVICE START
 systemctl start mysqld
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
- echo FAILURE
- fi
+STAT $?
 
 echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
 
