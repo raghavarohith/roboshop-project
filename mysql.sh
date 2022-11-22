fbe7592ae99e8b7974d3e965e5108c5da1d89561
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-23 01:17:04 +0400

    my sql script

--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,43 @@
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+if [ $? eq 0 ]
+then
+  echo SUCCESS
+else
+ echo FAILURE
+ fi
 dnf module disable mysql
+if [ $? eq 0 ]
+then
+  echo SUCCESS
+else
+ echo FAILURE
+ fi
 yum install mysql-community-server -y
+if [ $? eq 0 ]
+then
+  echo SUCCESS
+else
+ echo FAILURE
+ fi
 systemctl enable mysqld
+if [ $? eq 0 ]
+then
+  echo SUCCESS
+else
+ echo FAILURE
+ fi
 systemctl start mysqld
+if [ $? eq 0 ]
+then
+  echo SUCCESS
+else
+ echo FAILURE
+ fi
 
-echo
+echo show databases | mysql -uroot -pRoboShop@1
+
+if [ $? -ne 0]; then
 echo "ALTER USER 'root'@'localhost' IDENTIFIED by 'RoboShop@1';" >/tmp/root-pass-sql
 DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 cat /tmp/root-pass-sql | mysql --connect-expired-password  -uroot -p"${DEFAULT_PASSWORD}"
+fi
