014bb3e835dceba2224170ad0fcc4fea36880edc
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-23 22:57:09 +0400

    my sql script

--- cart.sh
+++ cart.sh
@@ -2,6 +2,7 @@ source common.sh
 PRINT "INSTALL NODEJS REPOS"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash >/tmp/log.txt
 STAT $?
+
 PRINT "install nodejs"
 yum install nodejs -y
 STAT $?
@@ -9,13 +10,13 @@ STAT $?
 PRINT "Adding application user"
 useradd roboshop
 STAT $?
+
 PRINT "download app content"
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" >/tmp/log.txt
 STAT $?
 
 PRINT "remove previous "
 cd /home/roboshop
-
 rm -rf cart
 STAT $?
 
@@ -30,9 +31,18 @@ PRINT "install dependencies"
 npm install >/tmp/log.txt
 STAT $?
 
-
+PRINT "configuration endpoints"
 sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' systemd.service
+STAT $?
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service >/tmp/log.txt
+PRINT "system reload"
 systemctl daemon-reload
+STAT $?
+
+PRINT "enable"
 systemctl enable cart
+STAT $?
+
+PRINT "restart"
 systemctl restart cart
+STAT $?
