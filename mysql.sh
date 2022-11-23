c452f7a248c4ae2b17199cbcfe766a45905e3b23
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-23 22:09:57 +0400

    my sql script

--- mysql.sh
+++ mysql.sh
@@ -3,7 +3,7 @@ if [ -z "$1" ]; then
   exit
   fi
 
-ROBOSHOP_MYSQL_PASSWORD=$1
+ROBOSHOP_MYSQL_PASSWORD=$1 &>>/tmp/log
 STAT () {
   if [ $1 -eq 0 ]
   then
@@ -16,20 +16,20 @@ PRINT (){
   echo -e "\e[34m$1\e[0m"
 }
 
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>/tmp/log
 STAT $?
  PRINT "DISABLE MYSQL SERVICE"
-dnf module disable mysql
+dnf module disable mysql &>>/tmp/log
 STAT $?
  PRINT "INSTALL MYSQL SERVICE"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>/tmp/log
 STAT $?
  PRINT "MYSQL SERVICE ENABLE"
-systemctl enable mysqld
+systemctl enable mysqld &>>/tmp/log
 STAT $?
 
  PRINT "MYSQL SERVICE START"
-systemctl start mysqld
+systemctl start mysqld &>>/tmp/log
 STAT $?
 
 echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
