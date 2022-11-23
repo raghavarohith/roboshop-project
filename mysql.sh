bcb9d0c0bf0012d90ce3312e2696e71b3b1a6cec
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-23 22:37:16 +0400

    my sql script

--- mysql.sh
+++ mysql.sh
@@ -35,7 +35,7 @@ STAT $?
 echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>/tmp/log
 
 if [ $? -ne 0 ]; then
-echo "ALTER USER 'root'@'localhost' IDENTIFIED by '${ROBOSHOP_MYSQL_PASSWORD}@1';" >/tmp/root-pass-sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED by '${ROBOSHOP_MYSQL_PASSWORD}';" >/tmp/root-pass-sql
 DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 cat /tmp/root-pass-sql | mysql --connect-expired-password  -uroot -p"${DEFAULT_PASSWORD}"
 fi
