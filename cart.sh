f243a01fba32c1aadb8f6514e337bd2fff8466ba
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-23 23:03:27 +0400

    my sql script

--- cart.sh
+++ cart.sh
@@ -1,12 +1,9 @@
 source common.sh
 PRINT "INSTALL NODEJS REPOS"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash >/tmp/log.txt
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 STAT $?
 
 PRINT "install nodejs"
-if [ $? -ne 0 ]; then
-  echo ALREADY EXITS
-  fi
 yum install nodejs -y
 STAT $?
 
@@ -15,7 +12,7 @@ useradd roboshop
 STAT $?
 
 PRINT "download app content"
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" >/tmp/log.txt
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
 STAT $?
 
 PRINT "remove previous "
@@ -24,20 +21,20 @@ rm -rf cart
 STAT $?
 
 PRINT "extracting app content"
-unzip -o /tmp/cart.zip >/tmp/log.txt
+unzip -o /tmp/cart.zip
 STAT $?
 
 mv cart-main cart
 cd cart
 
 PRINT "install dependencies"
-npm install >/tmp/log.txt
+npm install
 STAT $?
 
 PRINT "configuration endpoints"
 sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' systemd.service
 STAT $?
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service >/tmp/log.txt
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
 PRINT "system reload"
 systemctl daemon-reload
 STAT $?
