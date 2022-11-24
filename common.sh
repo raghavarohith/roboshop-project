2934d10f90ba25f4d57b9daed0a8c450ce36c654
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-24 20:14:34 +0400

    paymrnt iscript

--- common.sh
+++ common.sh
@@ -140,5 +140,9 @@ pip3 install -r requirements.txt &>>{LOG}
 STAT $?
 exit
 
+USER_ID=$(id -u roboshop)
+GROUP_ID=$(id -g roboshop)
+sed -i -e "/uid/ c uid =${USER_ID}" -e "/gid/ c gid ="${GROUP_ID}" /${COMPONENT}.ini
+
 SYSTEMD_SETUP
 }
