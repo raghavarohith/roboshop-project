7513f07a64578cb4230ffbac267eae5d8dc83bde
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-24 17:14:54 +0400

    mongoiscript

--- mongo.sh
+++ mongo.sh
@@ -1,32 +1,29 @@
-
 COMPONENT=mongodb
 source common.sh
 
-PRINT "MONGO REPO FILE"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG}
+PRINT "download repo file"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>{LOG}
 STAT $?
 
-PRINT "install mongo db"
-yum install -y mongodb-org &>>${LOG}
+PRINT "install mongodb"
+yum install -y mongodb-org &>>{LOG}
 STAT $?
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+PRINT "enable monodb"
+ systemctl enable mongod &>>{LOG}
+ STAT $?
+PRINT "start mongodb"
+systemctl restart mongod &>>{LOG}
+STAT $?
+
+PRINT "update listen ip"
+se -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>{LOG}
+STAT $?
+
 APP_LOC=/tmp
 CONTENT=mongodb-main
 DOWNLOAD_APP_CODE
-cd mongodb-main
-PRINT "mongo catalogue"
-mongo < catalogue.js &>>${LOG}
-STAT $?
 
-PRINT "mongo user"
-mongo < users.js &>>${LOG}
-STAT $?
-
-PRINT "enable mongodb"
-systemctl enable mongod &>>${LOG}
-STAT $?
-
-PRINT "restart mongodb"
-systemctl restart mongod &>>${LOG}
-STAT $?
+cd mongodb-main &>>{LOG}
+ mongo < catalogue.js &>>{LOG}
+mongo < users.js &>>{LOG}
