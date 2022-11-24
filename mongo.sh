45fe5e30554ca294dc62108a89046c0eb011424f
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-24 16:17:24 +0400

    mongoiscript

--- mongo.sh
+++ mongo.sh
@@ -7,7 +7,7 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/robos
 STAT $?
 
 PRINT "install mongo db"
-yum install -y mongodb- &>>${LOG}
+yum install -y mongodb-org &>>${LOG}
 STAT $?
 
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.
