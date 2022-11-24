088c73a6b1733641ea05ff5d44ad77b6e47d91b5
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-24 17:03:02 +0400

    mongoiscript

--- mongo.sh
+++ mongo.sh
@@ -12,8 +12,9 @@ STAT $?
 
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 APP_LOC=/tmp
-CONTENT=$COMPONENT-main
+CONTENT=mongodb-main
 DOWNLOAD_APP_CODE
+cd mongodb-main
 PRINT "mongo catalogue"
 mongo < catalogue.js &>>${LOG}
 STAT $?
