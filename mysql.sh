ef5d08d7356f21b5736c4280567d8ec7c2809049
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-25 19:05:42 +0400

    paymrnt iscript

--- mysql.sh
+++ mysql.sh
@@ -1,35 +1,31 @@
-COMPONENT=mysql
-source common.sh
 
 if [ -z "$1" ]; then
   echo INPUT PASSWORD IS
   exit 1
   fi
 
+  COMPONENT=mysql
+  source common.sh
+
 ROBOSHOP_MYSQL_PASSWORD=$1 &>>/tmp/log
-STAT () {
-  if [ $1 -eq 0 ]
-  then
-    echo "SUCCESS"
-  else
-   echo "FAILURE""
-   fi
-}
-PRINT (){
-  echo -e "\e[34m$1\e[0m"
-}
+
+
 PRINT "DOWNLOADING MYSQL REPO FILE"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>/tmp/log
 STAT $?
+
  PRINT "DISABLE MYSQL SERVICE"
 dnf module disable mysql &>>/tmp/log
 STAT $?
+
  PRINT "INSTALL MYSQL SERVICE"
 yum install mysql-community-server -y &>>/tmp/log
 STAT $?
+
  PRINT "MYSQL SERVICE ENABLE"
 systemctl enable mysqld &>>/tmp/log
 STAT $?
+
 
  PRINT "MYSQL SERVICE START"
 systemctl start mysqld &>>/tmp/log
