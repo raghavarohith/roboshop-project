1dd9f93eb34eaaf45ae9995123bb76067bd98e2b
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-25 18:48:52 +0400

    paymrnt iscript

--- frontend.sh
+++ frontend.sh
@@ -2,13 +2,20 @@ COMPONENT=frontend
 source common.sh
 
 PRINT "install nginx"
-yum install nginx -y
+yum install nginx -y &>>{LOG}
 STAT $?
 
 APP_LOC=/usr/share/nginx/html
 
 DOWNLOAD_APP_CODE
-mv frontend-main/static/* .
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-systemctl enable nginx
-systemctl restart nginx
+
+mv frontend-main/static/* . &>>{LOG}
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>{LOG}
+
+PRINT "enable nginx"
+systemctl enable nginx &>>{LOG}
+STAT $?
+
+PRINT "restart nginx"
+systemctl restart nginx &>>{LOG}
+STAT $?
