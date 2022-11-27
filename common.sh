d10ab431ba7a7493fc9d55aae4e471b301bf4963
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-27 12:19:37 +0400

    nginx iscript

--- common.sh
+++ common.sh
@@ -152,9 +152,17 @@ DOWNLOAD_APP_CODE
 mv ${COMPONENT}-main ${COMPONENT} &>>${LOG}
 cd ${COMPONENT} &>>${LOG}
 
+PRINT "GO init"
 go mod init dispatch &>>${LOG}
+STAT $?
+
+PRINT "GO GET"
 go get &>>${LOG}
+STAT $?
+
+PRINT "GO BUILD"
 go build &>>${LOG}
+STAT $?
 
 
 SYSTEMD_SETUP
