08374f0c02814856f4e54fedc4e1347e6fd6e8b8
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-27 10:39:45 +0400

    mongo iscript

--- mongo.sh
+++ mongo.sh
@@ -1,31 +1,36 @@
 COMPONENT=mongodb
 source common.sh
 
-PRINT "download repo file"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>{LOG}
+PRINT "Download YUM Repo File"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG
 STAT $?
 
-PRINT "install mongodb"
-yum install -y mongodb-org &>>{LOG}
+PRINT "Install MongoDB"
+yum install -y mongodb-org &>>$LOG
 STAT $?
 
-PRINT "update listen ip"
-sed -i -e '/s 127.0.0.1/0.0.0.0' /etc/mongod.conf &>>{LOG}
+PRINT "Configure MongoDB Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG
 STAT $?
 
-PRINT "enable monodb"
-systemctl enable mongod &>>{LOG}
+PRINT "Enable MongoDB"
+systemctl enable mongod &>>$LOG
 STAT $?
 
-PRINT "start mongodb"
-systemctl restart mongod &>>{LOG}
+PRINT "Start MongoDB"
+systemctl restart mongod &>>$LOG
 STAT $?
 
 APP_LOC=/tmp
 CONTENT=mongodb-main
-
 DOWNLOAD_APP_CODE
 
-cd mongodb-main &>>{LOG}
- mongo < catalogue.js &>>{LOG}
-mongo < users.js &>>{LOG}
+cd mongodb-main &>>$LOG
+
+PRINT "Load Catalogue Schema"
+mongo < catalogue.js &>>$LOG
+STAT $?
+
+PRINT "Load Users Schema"
+mongo < users.js &>>$LOG
+STAT $?
