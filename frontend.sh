9a71f820e63376b3cc01dcddfe18d75c668ff5e1
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-27 11:48:09 +0400

    nginx iscript

--- frontend.sh
+++ frontend.sh
@@ -13,7 +13,7 @@ mv frontend-main/static/* . &>>{LOG}
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>{LOG}
 
 PRINT "Update RoboShop Configuration"
-sed -i -e '/catalogue/ s/localhost/dev-catalogue.mydevops410.online/' -e '/user/ s/localhost/dev-user.mydevops410.online/' -e '/cart/ s/localhost/dev-cart.mydevops410.online/' /etc/nginx/default.d/roboshop.conf
+sed -i -e '/catalogue/ s/localhost/dev-catalogue.mydevops410.online/' -e '/user/ s/localhost/dev-user.mydevops410.online/' -e '/cart/ s/localhost/dev-cart.mydevops410.online/' -e '/shipping/ s/localhost/dev-shipping.mydevops410.online/' -e '/payment/ s/localhost/dev-payment.mydevops410.online/' /etc/nginx/default.d/roboshop.conf
 STAT $?
 
 PRINT "enable nginx"
